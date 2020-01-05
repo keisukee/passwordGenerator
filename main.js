68370509b081c7df06d3a550c16a2d86beee357c
keisuke ohashi <kog71903@gmail.com>
2020-01-05 17:16:28 +0900

    [update]パスワードジェネレーター、アルファベットだけ版完成

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 passwordDisplay = document.getElementById('password');
+passwordLengthInput = document.getElementById('password-length');
+passwordGenerateBtn = document.getElementById('generate-btn');
 
-// passwordDisplay.value = 'hogehoge';
-
-function generatePassword(element, length) {
+const generatePassword = (element, length) => {
   let alphabets = 'abcdefghijklmnopqrstuvwxyz';
   let password = '';
 
   for (let i = 0; i < length; i++) {
     let n = Math.floor(Math.random() * alphabets.length);
-    console.log("random", n);
     let randomLetter = alphabets[n];
-    console.log("letter", randomLetter);
     password += randomLetter;
   }
   element.value = password;
 }
 
-generatePassword(passwordDisplay, 8);
+passwordGenerateBtn.addEventListener('click', () => {
+  let passwordLength = passwordLengthInput.value;
+  if (isNaN(passwordLength) || (Number(passwordLength) <= 0)) {
+    generatePassword(passwordDisplay, 10);
+  } else {
+    generatePassword(passwordDisplay, parseInt(passwordLength));
+  }
+});
+
